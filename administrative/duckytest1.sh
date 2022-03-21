cfc439938abe06c570118f5610e612397da5a157
Giptech <85649942+Giptech@users.noreply.github.com>
2022-03-21 15:06:46 -0400

    Create duckytest1.sh

---
+++ administrative/duckytest1.sh
@@ -0,0 +1,13 @@
+sudo apt install git                                 
+git clone https://github.com/micronucleus/micronucleus                     
+cd micronucleus/commandline/
+sudo apt install libusb-dev
+make
+cp micronucleus ~/.arduino15/packages/digistump/tools/micronucleus/2.0a4
+
+sudo cp 49-micronucleus.rules /etc/udev/rules.d/. 
+
+git clone https://github.com/digistump/avr-dummy
+cd avr-dummy
+make
+cp avrdude ~/.arduino15/packages/digistump/tools/micronucleus/2.0a4/launcher
