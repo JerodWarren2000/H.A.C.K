b29fd736f183a64e3657ae8cf933604799fd2c13
realblopez <91297972+realblopez@users.noreply.github.com>
2022-03-23 13:42:13 -0400

    Update ArduinoInstallScript.sh
    line 4: changed kali to HACKUser

--- lab6-rubberducky/ArduinoInstallScript.sh
+++ lab6-rubberducky/ArduinoInstallScript.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https://downloads.arduino.cc/arduino-1.8.19-linuxarm.tar.xz
 sudo tar -xf arduino-1.8.19-linuxarm.tar.xz
-sudo /home/kali/arduino-1.8.19/install.sh
+sudo /home/HACKUser/arduino-1.8.19/install.sh
