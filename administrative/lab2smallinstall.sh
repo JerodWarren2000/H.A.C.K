e5d6af3d5e12d15a72683921da8cbd855d65954b
Giptech <85649942+Giptech@users.noreply.github.com>
2022-03-30 16:19:34 -0400

    Create lab2smallinstall.sh

---
+++ administrative/lab2smallinstall.sh
@@ -0,0 +1,6 @@
+# Lab 2 Installation
+mkdir ~/Lab2
+mkdir ~/Documents
+wget https://git.lysator.liu.se/catears/beemovie/-/raw/master/beemovie.txt?inline=false -O ~/Lab2/searchme.txt 
+echo “Great Job on getting this far in the lab. Take a break if you need to!” > ~/Lab2/deleteme.txt
+wget https://filesamples.com/samples/document/txt/sample3.txt -O ~/Documents/findme.txt
